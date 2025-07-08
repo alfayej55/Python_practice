2793ba48d25592614780c76ca814a4f142ace2f9
MD:Al-Fayej <mdal-fayej@MDAl-Fayejs-Mac-mini.local>
2025-07-08 23:14:03 +0600

    input variable check and in oparetor

--- spesal_oparetor_check.py
+++ spesal_oparetor_check.py
@@ -4,3 +4,15 @@
 da="Bangladesh is a independet country"
 
 print("independet" in da)
+
+# Input variable And in oparetor check
+ 
+name=['fayej','palas','sakib','hasin']
+
+data=input("Enter naem>>>: ")
+
+if data in name :
+
+    print(f'{data} is present' )
+else :
+    print('not present')
