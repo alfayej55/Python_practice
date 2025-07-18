1c7524099dc57c4f7de26293fcc06dd806e97e2f
MD:Al-Fayej <mdal-fayej@MDAl-Fayejs-Mac-mini.local>
2025-07-19 00:42:40 +0600

    and or oparetor

--- spesal_oparetor_check.py
+++ spesal_oparetor_check.py
@@ -16,3 +16,16 @@ if data in name :
     print(f'{data} is present' )
 else :
     print('not present')
+
+
+
+    # And / or Oparetor practice
+
+
+exprince=input("Do you habe privius exprince? y/n: ")
+company= int(input("How much company you do: "))
+
+if(exprince=='y' and company==2):
+    print("select>>>")
+else :
+    print("not select")
